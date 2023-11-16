048587f047b81c05f34636dde0411e413911ed6d
Bill Lewis <wrljet@gmail.com>
2023-11-16 17:48:18 -0500

    Formula v0.9.8, SDL-Hercules-390 build 4.6.0.10941-SDL-g65c97fd6

--- sdl-hercules-develop.rb
+++ sdl-hercules-develop.rb
@@ -4,7 +4,7 @@
 class SdlHerculesDevelop < Formula
   desc "SDL-Hercules-390 Develop Branch, under The Q Public License"
   homepage "https://github.com/wrljet/sdl-hercules-develop-homebrew"
-  url "https://github.com/wrljet/sdl-hercules-binaries-macos/archive/refs/tags/v0.9.7.tar.gz"
+  url "https://github.com/wrljet/sdl-hercules-binaries-macos/archive/refs/tags/v0.9.8.tar.gz"
   sha256 ""
   license "QPL-1.0"
 
